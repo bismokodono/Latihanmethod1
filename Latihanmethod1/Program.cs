7f83a060c3160a2035195f40390bdd4906a24a8a
bismokodono <bismokodono@gmail.com>
2026-01-21 13:28:22 +0700

    menambahkan method tambah kurang kali bagi

--- Latihanmethod1/Program.cs
+++ Latihanmethod1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 10, b = 2, c = 0;
+
+void Tambah()
+{
+    c = a + b;
+    Console.WriteLine($"{a} + {b} = {c}");
+}
+void Kurang()
+{
+    c = a - b;
+    Console.WriteLine($"{a} - {b} = {c}");
+}
+void Kali()
+{
+    c = a * b;
+    Console.WriteLine($"{a} * {b} = {c}");
+}
+void Bagi()
+{
+    c = a / b;
+    Console.WriteLine($"{a} / {b} = {c}");
+}
