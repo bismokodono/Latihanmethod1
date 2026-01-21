b21c9ac4269a2b5d1286f105926da0b2941a9be2
bismokodono <bismokodono@gmail.com>
2026-01-21 13:30:44 +0700

    parameter dan argumen

--- Latihanmethod1/Program.cs
+++ Latihanmethod1/Program.cs
@@ -1,27 +1,27 @@
-﻿int a = 10, b = 2, c = 0;
+﻿int c = 0;
 
-void Tambah()
+void Tambah(int a, int b)
 {
     c = a + b;
     Console.WriteLine($"{a} + {b} = {c}");
 }
-void Kurang()
+void Kurang(int a, int b)
 {
     c = a - b;
     Console.WriteLine($"{a} - {b} = {c}");
 }
-void Kali()
+void Kali(int a, int b)
 {
     c = a * b;
     Console.WriteLine($"{a} * {b} = {c}");
 }
-void Bagi()
+void Bagi(int a, int b)
 {
     c = a / b;
     Console.WriteLine($"{a} / {b} = {c}");
 }
 
-Tambah();
-Kurang();
-Kali();
-Bagi();
+Tambah(8,2);
+Kurang(5,2);
+Kali(3,10);
+Bagi(8,4);
